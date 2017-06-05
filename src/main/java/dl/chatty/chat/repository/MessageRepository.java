768067d62d3235ee4a2752d9c60f12a86fafa428
Daniel Las <lasdaniel@gmail.com>
2017-06-05 07:18:01 +0200

    Transactional Message repository method

--- src/main/java/dl/chatty/chat/repository/MessageRepository.java
+++ src/main/java/dl/chatty/chat/repository/MessageRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import dl.chatty.chat.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    @Transactional(readOnly = true)
     @Query("select m from Message m where chat.id=:chatId order by id")
     List<Message> findByChatIdOrderById(@Param("chatId") Long chatId);
 }
